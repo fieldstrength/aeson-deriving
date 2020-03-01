7a0436399e549781e9435f90ba4f553fc9f40e38
Cliff Harvey <cs.hbar@gmail.com>
2020-03-02 00:07:48 +0100

    Add LoopWarning constraint family

--- src/Data/Aeson/Deriving/WithConstantFields.hs
+++ src/Data/Aeson/Deriving/WithConstantFields.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Data.Aeson.Deriving.WithConstantFields where
 
@@ -36,7 +37,7 @@ instance (ToConstantObject fields, KnownSymbol key, ToConstant val)
         (toConstant $ Proxy @val)
         (toConstantObject $ Proxy @fields)
 
-instance (ToJSON a, ToConstantObject obj) => ToJSON (WithConstantFields obj a) where
+instance (ToJSON a, LoopWarning a, ToConstantObject obj) => ToJSON (WithConstantFields obj a) where
   toJSON (WithConstantFields x) = mapObjects (<> fields) $ toJSON x
     where
       fields = toConstantObject $ Proxy @obj
@@ -45,7 +46,7 @@ instance (ToJSON a, ToConstantObject obj) => ToJSON (WithConstantFields obj a) w
       mapObjects f (Object o) = Object (f o)
       mapObjects _ val        = val
 
-instance (FromJSON a, ToConstantObject obj) => FromJSON (WithConstantFields obj a) where
+instance (FromJSON a, LoopWarning a, ToConstantObject obj) => FromJSON (WithConstantFields obj a) where
   parseJSON valIn = WithConstantFields <$>
     parseJSON valIn <*
       HashMap.traverseWithKey assertFieldPresent (toConstantObject $ Proxy @obj)
