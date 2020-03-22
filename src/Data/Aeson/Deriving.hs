4acadfd75c9894cc2a8b3f4c8bbc6cb3d8bf4a69
Cliff Harvey <cs.hbar@gmail.com>
2020-03-22 19:29:13 +0100

    Move RecordSum to Generic module to allow it in LoopWarning constraint
    As it, along with GenericEncoded, are the two newtypes that do not
    modify instances recursively, they are the ones safe to use for the base
    type of a derived aeson instance.

--- src/Data/Aeson/Deriving.hs
+++ src/Data/Aeson/Deriving.hs
@@ -1,6 +1,5 @@
 module Data.Aeson.Deriving (module All) where
 
 import Data.Aeson.Deriving.Generic as All
-import Data.Aeson.Deriving.RecordSum as All
 import Data.Aeson.Deriving.SingleFieldObject as All
 import Data.Aeson.Deriving.WithConstantFields as All
