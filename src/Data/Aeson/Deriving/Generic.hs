90f1c08a83b6adf423cf12ae9ba8efb3b69c3c62
Cliff Harvey <cs.hbar@gmail.com>
2020-03-22 19:29:13 +0100

    Move RecordSum to Generic module to allow it in LoopWarning constraint
    As it, along with GenericEncoded, are the two newtypes that do not 
    modify instances recursively, they are the ones safe to use for the base 
    type of a derived aeson instance.

--- src/Data/Aeson/Deriving/Generic.hs
+++ src/Data/Aeson/Deriving/Generic.hs
@@ -1,12 +1,13 @@
+{-# LANGUAGE PolyKinds            #-}
+{-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE PolyKinds #-}
 
 module Data.Aeson.Deriving.Generic
-  ( -- ** Main data type
-    GenericEncoded(..)
-  -- ** Main typeclass
-  , ToAesonOptions(..)
+  ( -- ** Main typeclass
+    ToAesonOptions(..)
+ -- ** Main data types for generics-based encodings
+  , GenericEncoded(..)
+  , RecordSumEncoded(..)
   -- **** Helper data type for Options definitions
   , GenericOptions
   -- ** Fields of Aeson Options
@@ -40,9 +41,15 @@ module Data.Aeson.Deriving.Generic
   ) where
 
 import           Data.Aeson
-import           Data.Char (toLower, toUpper, isUpper)
-import           Data.Kind (Type, Constraint)
-import           Data.Proxy (Proxy(..))
+import           Data.Aeson.Deriving.RecordSum.Internal
+import           Data.Aeson.Types                       (modifyFailure)
+import           Data.Char                              (isUpper, toLower, toUpper)
+import           Data.Function                          ((&))
+import qualified Data.HashMap.Strict                    as HashMap
+import           Data.Kind                              (Constraint, Type)
+import           Data.List                              (intercalate)
+import           Data.Proxy                             (Proxy (..))
+import           Data.Text                              (pack)
 import           GHC.Generics
 import           GHC.TypeLits
 
@@ -77,6 +84,7 @@ class ToAesonOptions a where
 --   of such a newtype over the naked base type instead of the 'GenericEncoded' version.
 type family LoopWarning a :: Constraint where
   LoopWarning (GenericEncoded opts a) = ()
+  LoopWarning (RecordSumEncoded tagKey tagValMod a) = ()
   LoopWarning (f x) = LoopWarning x
   LoopWarning x = TypeError
     ( 'Text "Uh oh! Watch out for those infinite loops!"
@@ -84,6 +92,68 @@ type family LoopWarning a :: Constraint where
     ':$$: 'Text "Instead your type is based directly on:  " ':<>: 'ShowType x
     ':$$: 'Text "You probably created an infinitely recursive encoder/decoder pair."
     )
+
+
+------------------------------------------------------------------------------------------
+-- Sums over records
+------------------------------------------------------------------------------------------
+
+-- | An encoding scheme for sums of records that are defined as distinct data types.
+--   If we have a number of record types we want to combine under a sum, a straightforward
+--   solution is to ensure that each each inner type uses a constructor tag, and then
+--   derive the sum with @SumEncoding := UntaggedValue@. This works fine for the happy
+--   path, but makes for very bad error messages, since it means that decoding proceeds by
+--   trying each case in sequence. Thus error messages always pertain to the last type in
+--   the sum, even when it wasn't the intended payload. This newtype improves on that
+--   solution by providing the relevant error messages, by remembering the correspondence
+--   between the constructor tag and the intended inner type/parser.
+--
+--   In order to work correctly, the inner types must use the 'TaggedObject' encoding.
+--   The same tag field name and 'ConstructorTagModifier' must be supplied to this type.
+newtype RecordSumEncoded (tagKey :: Symbol) (tagModifier :: k) (a :: Type) = RecordSumEncoded a
+
+instance
+  ( Generic a
+  , GFromJSON Zero (Rep a)
+  , GTagParserMap (Rep a)
+  , Rep a ~ D1 meta cs
+  , Datatype meta
+  , StringFunction tagModifier
+  , KnownSymbol tagKey)
+    => FromJSON (RecordSumEncoded tagKey tagModifier a) where
+      parseJSON val = prependErrMsg outerErrorMsg . flip (withObject "Object") val $ \hm -> do
+        tagVal <- hm .: pack tagKeyStr
+        case HashMap.lookup tagVal parserMap of
+          Nothing -> fail . mconcat $
+            [ "We are not expecting a payload with tag value " <> backticks tagVal
+            , " under the " <> backticks tagKeyStr <> " key here. "
+            , "Expected tag values: "
+            , intercalate ", " $ backticks <$> HashMap.keys parserMap
+            , "."
+            ]
+          Just parser -> RecordSumEncoded . to <$> parser val
+            & prependErrMsg
+              ("Failed parsing the case with tag value "
+                <> backticks tagVal <> " under the "
+                <> backticks tagKeyStr <> " key: ")
+
+        where
+          tagKeyStr = symbolVal $ Proxy @tagKey
+          ParserMap parserMap
+            = unsafeMapKeys (stringFunction $ Proxy @tagModifier)
+            . gParserMap
+            $ Proxy @(Rep a)
+          backticks str = "`" <> str <> "`"
+          prependErrMsg str = modifyFailure (str <>)
+          outerErrorMsg = "Failed to parse a " <> datatypeName @meta undefined <> ": "
+
+
+instance
+  ( Generic a
+  , GToJSON Zero (Rep a))
+    => ToJSON (RecordSumEncoded tagKey tagModifier a) where
+      toJSON (RecordSumEncoded x) =
+        toJSON $ GenericEncoded @'[SumEncoding := UntaggedValue] x
 
 
 ------------------------------------------------------------------------------------------
